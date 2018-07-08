4dbf4d9819d350faa456ab0026c78578fe0d6e8c
pengsong <570631809@qq.com>
2018-07-08 12:14:32 +0800

    debug

--- src/test/java/com/example/demo/SpringbootdemoApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootdemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.dao.RedisDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,11 +15,8 @@ public class SpringbootdemoApplicationTests {
     public void contextLoads() {
     }
 
-    private final RedisDao redisDao;
-
-    public SpringbootdemoApplicationTests(RedisDao redisDao) {
-        this.redisDao = redisDao;
-    }
+    @Autowired
+    RedisDao redisDao;
 
     @Test
     public void testRedis() {
