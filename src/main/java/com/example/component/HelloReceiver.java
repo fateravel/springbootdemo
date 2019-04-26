a7270915cf96e363a8e5c8bef6f9a8b9e42ac48e
pengsong <ps5317@163.com>
2019-04-26 18:58:00 +0800

    rabbitmq

--- src/main/java/com/example/component/HelloReceiver.java
+++ src/main/java/com/example/component/HelloReceiver.java
@@ -3,17 +3,18 @@ package com.example.component;
 import com.example.entity.User;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
  * @author pengsong
  */
-//@Component
-//@RabbitListener(queues = "hello")
+@Component
+//@RabbitListener(queues = "hello", containerFactory = "rabbitListenerContainerFactory")
 public class HelloReceiver {
 
-    @RabbitHandler
-    public void process(User user) {
+    @RabbitListener(queues = "hello")
+    public void process(@Payload User user) {
         System.out.println("Receive:" + user.toString());
     }
 }
--- src/main/java/com/example/config/RabbitMqConfig.java
+++ src/main/java/com/example/config/RabbitMqConfig.java
@@ -1,6 +1,11 @@
 package com.example.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +19,21 @@ public class RabbitMqConfig {
     @Bean
     public Queue helloQueue() {
         return new Queue("hello");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 }
