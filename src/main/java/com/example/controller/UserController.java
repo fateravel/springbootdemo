f16ac943552ff4ac119c109b8cd14a28c0ffa83a
pengsong <ps5317@163.com>
2019-08-21 14:14:20 +0800

    add log

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -2,14 +2,19 @@ package com.example.controller;
 
 import com.example.entity.User;
 import com.example.service.UserService;
+import com.example.util.JsonUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/user")
 @RestController
 @Api("用户表")
 public class UserController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final UserService userService;
 
@@ -20,12 +25,15 @@ public class UserController {
     @GetMapping("/{username}")
     @ApiOperation("获取用户名")
     public User getUser(@PathVariable("username") String username) {
-        return userService.findByUsername(username);
+        User user = userService.findByUsername(username);
+        logger.info("user info :" + JsonUtil.serialize(user));
+        return user;
     }
 
     @PostMapping("/add")
     @ApiOperation("添加用户")
     public void addUser(@RequestBody User user) {
+        logger.info("add user info: " + JsonUtil.serialize(user));
         userService.addUser(user);
     }
 }
